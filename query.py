d1dd7d161924edc96a03b9b181766499bbff1a81
0x10F8 <cgardi203@caledonian.ac.uk>
2019-08-06 17:20:15 +0100

    Updated

--- query.py
+++ query.py
@@ -23,15 +23,25 @@ with open(DATA_FILE, 'r', 1024) as data:
     symbol_only = 0
     number_only = 0
 
-    alpha = 0
-    alpha_numeric = 0
+    alpha_only = 0
+    alpha_numeric_only = 0
+
+    lengths = {}
+
+    patterns = {}
+
+    dictionary_count = {}
 
     for line in data.readlines():
         json_data = loads(line)
+        password = json_data['password']
         lower = json_data['lower']
         upper = json_data['upper']
         numeric = json_data['numeric']
         symbol = json_data['symbol']
+        sequences = json_data['sequence']
+        length = len(password)
+
         if lower:
             lower_count += 1
         if upper:
@@ -50,7 +60,44 @@ with open(DATA_FILE, 'r', 1024) as data:
         if numeric and not lower and not symbol and not upper:
             number_only += 1
 
+        if (lower or upper) and not numeric and not symbol:
+            alpha_only += 1
+        if (lower or upper) and numeric and not symbol:
+            alpha_numeric_only += 1
+
+        if length in lengths.keys():
+            lengths[length] += 1
+        else:
+            lengths[length] = 1
+
+        for sequence in sequences:
+            pattern = sequence['pattern']
+            if pattern in patterns.keys():
+                patterns[pattern] += 1
+            else:
+                patterns[pattern] = 1
+            if pattern == "dictionary":
+                matched_word = sequence['matched_word']
+                if matched_word in dictionary_count.keys():
+                    dictionary_count[matched_word] += 1
+                else:
+                    dictionary_count[matched_word] = 1
+
     palog.log("Lower: {0} Upper: {1} Number: {2} Symbol: {3}".format(
         lower_count, upper_count, number_count, symbol_count))
     palog.log("LowerOnly: {0} UpperOnly: {1} NumberOnly: {2} SymbolOnly: {3}".format(
         lower_only, upper_only, number_only, symbol_only))
+    palog.log("AlphaOnly: {0} AlphaNumeric: {1}".format(
+        alpha_only, alpha_numeric_only))
+
+    for length in sorted(lengths.keys()):
+        palog.log(
+            "Length distribution: {0} - {1}".format(length, lengths[length]))
+
+    for pattern in patterns.keys():
+        palog.log("Pattern {0}:{1}".format(pattern, patterns[pattern]))
+
+    for word, count in sorted(dictionary_count.items(), key=lambda x: x[1], reverse=True):
+        if count > 50:
+            palog.log("Dictionary word matched {0}:{1}".format(
+                word, count))
